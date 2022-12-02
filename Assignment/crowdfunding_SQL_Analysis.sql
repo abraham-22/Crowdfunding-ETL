8a94b6b7f2fc9444466aa767e77540e169f0f419
abraham-22 <114262970+abraham-22@users.noreply.github.com>
2022-12-01 22:02:22 -0600

    ETL Final

--- Assignment/crowdfunding_SQL_Analysis.sql
+++ Assignment/crowdfunding_SQL_Analysis.sql
@@ -9,42 +9,60 @@ ORDER BY cf_id DESC;
 -- 2. (2.5 pts)
 -- Using the "backers" table confirm the results in the first query.
 
-SELECT * FROM campaign ;
+SELECT * FROM backers ;
 
 -- 3. (5 pts)
 -- Create a table that has the first and last name, and email address of each contact.
 -- and the amount left to reach the goal for all "live" projects in descending order. 
-
--- Joining campaign and contact tables
 SELECT co.first_name,
 	co.last_name,
     co.email,
     ca.goal,
-    ca.pledged
-INTO live_campaign
+    ca.pledged,
+	(ca.goal-ca.pledged)AS remaining_goal_amount
+-- INTO email_contacts_remaining_goal_amount
 FROM contacts as co
 	INNER JOIN campaign as ca
 		ON (co.contact_id = ca.contact_id)
 ORDER BY ca.goal DESC;
 
-
-SELECT ca.goal- ca.pledged AS LEFT remaining_goal
-FROM live_campaign as lca 
-INNER JOIN campaign as ca
-		ON (lca.contact_id = ca.contact_id)
-GROUP BY ca.goal
-ORDER BY ca.goal DESC;
-
 -- Check the table
-SELECT * FROM live_campaign ;
+SELECT * FROM email_contacts_remaining_goal_amount ;
 
 -- 4. (5 pts)
 -- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
 -- and has the first and last name of each backer, the cf_id, company name, description, 
 -- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
-
-
+-- 1. Join Backers and Campaign table
+SELECT b.first_name,
+	b.last_name,
+    b.email,
+    ca.cf_id,
+    ca.company_name,
+	ca.description,
+	ca.end_date,
+	ca.goal,
+    ca.pledged
+INTO backers_new
+FROM backers as b
+	LEFT JOIN campaign as ca
+		ON (b.cf_id = ca.cf_id)
+;
+-- 2. Using the new table get the remaining amount of the campaign goal as "Left of Goal"
+SELECT bn.email,
+	bn.last_name,
+    bn.first_name,
+    bn.cf_id,
+    bn.company_name,
+	bn.description,
+	bn.end_date,
+	bn.goal,
+	bn.pledged,
+    (bn.goal-bn.pledged)AS remaining_goal_amount
+INTO email_backers_remaining_goal_amount
+FROM backers_new as bn
+ORDER BY bn.email DESC;
 
 -- Check the table
-
+SELECT * FROM email_backers_remaining_goal_amount ;
 
